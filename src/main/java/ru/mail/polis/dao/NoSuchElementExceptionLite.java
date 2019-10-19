720ded5fa2abca6cff73eacca60d758f6fea0b43
Yaroslav <seliverstov-yr@mail.ru>
2019-10-19 16:41:48 +0300

    fix

--- src/main/java/ru/mail/polis/dao/NoSuchElementExceptionLite.java
+++ src/main/java/ru/mail/polis/dao/NoSuchElementExceptionLite.java
@@ -2,6 +2,7 @@ package ru.mail.polis.dao;
 
 import java.util.NoSuchElementException;
 
+@SuppressWarnings("serial")
 public class NoSuchElementExceptionLite extends NoSuchElementException {
 
     NoSuchElementExceptionLite(final String s) {
@@ -9,7 +10,6 @@ public class NoSuchElementExceptionLite extends NoSuchElementException {
     }
 
     @Override
-    @SuppressWarnings("UnsynchronizedOverridesSynchronized")
     public Throwable fillInStackTrace() {
             return this;
     }
